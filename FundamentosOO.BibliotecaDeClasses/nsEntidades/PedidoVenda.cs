2a25f8795a245e95bfe90065399b80f2ff1167f2
Juliano Ribeiro de Souza Maciel <juliano.maciel@totvs.com.br>
2021-03-09 22:35:45 -0300

    Aula 09/03/2021

---
+++ FundamentosOO.BibliotecaDeClasses/nsEntidades/PedidoVenda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosOO.BibliotecaDeClasses.nsEntidades
+{
+    public class PedidoVenda
+    {
+        public string NomeCliente { get; private set; }
+        public string NomeVendedor { get; private set; }
+        public string Endereco { get; private set; }
+        public decimal ValorTotal { get; private set; }
+
+        public PedidoVenda(string nomeCliente, string nomeVendedor, string endereco, decimal valorTotal)
+        {
+            NomeCliente = nomeCliente;
+            NomeVendedor = nomeVendedor;
+            Endereco = endereco;
+            ValorTotal = valorTotal;
+        }
+
+        public override string ToString()
+        {
+            return $"Cliente: {NomeCliente} Vendedor: {NomeVendedor} Endereço: {Endereco} Total R$: {ValorTotal}";
+        }
+    }
+}
---
+++ FundamentosOO.Metodos/Program.cs
@@ -0,0 +1,71 @@
+﻿using FundamentosOO.BibliotecaDeClasses.nsEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosOO.Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pedido1 = new PedidoVenda("Juliano", "Juarez", "Rua Caravelas, 991, Jussara - PR", 100.60m);
+
+            #region Implementação convencional
+
+            ImprimirPedido(pedido1.NomeCliente, pedido1.NomeVendedor, pedido1.ValorTotal);
+
+            #endregion Implementação convencional
+
+            #region Parâmetros nomeados
+
+            ImprimirPedido(vendedor: pedido1.NomeVendedor, valorTotal: pedido1.ValorTotal, cliente: pedido1.NomeCliente);
+            ImprimirPedido(valorTotal: pedido1.ValorTotal, vendedor: pedido1.NomeVendedor, cliente: pedido1.NomeCliente);
+
+            #endregion Parâmetros nomeados
+
+            #region Parâmetro opcional
+
+            ImprimirPedidoComParametrosOpcionais(pedido1.NomeCliente, pedido1.ValorTotal);
+            ImprimirPedidoComParametrosOpcionais(pedido1.NomeCliente, pedido1.ValorTotal, pedido1.NomeVendedor);
+
+            #endregion Parâmetro opcional
+
+            #region Sobrecargas
+
+            ImprimirPedido(pedido1.NomeCliente, pedido1.NomeVendedor, pedido1.Endereco, pedido1.ValorTotal);
+            Console.WriteLine(ImprimirPedido(pedido1));
+            Console.WriteLine(pedido1.ToString());
+
+            #endregion Sobrecargas
+
+            Console.ReadLine();
+        }
+
+        internal static void ImprimirPedido(string cliente, string vendedor, decimal valorTotal)
+        {
+            Console.WriteLine($"Cliente: {cliente} Vendedor: {vendedor} Total R$: {valorTotal}");
+        }
+
+        internal static void ImprimirPedido(string cliente, string vendedor, string endereco, decimal valorTotal)
+        {
+            Console.WriteLine($"Cliente: {cliente} Vendedor: {vendedor} Endereço: {endereco} Total R$: {valorTotal}");
+        }
+
+        internal static string ImprimirPedido(PedidoVenda pedidoVenda)
+        {
+            return $"Cliente: {pedidoVenda.NomeCliente} Vendedor: {pedidoVenda.NomeVendedor}" +
+                $" Endereço: {pedidoVenda.Endereco} Total R$: {pedidoVenda.ValorTotal}";
+        }
+
+        internal static void ImprimirPedidoComParametrosOpcionais(string cliente, decimal valorTotal, string vendedor = "")
+        {
+            if (string.IsNullOrWhiteSpace(vendedor))
+                Console.WriteLine($"Cliente: {cliente} Total R$: {valorTotal}");
+            else
+                Console.WriteLine($"Cliente: {cliente} Vendedor: {vendedor} Total R$: {valorTotal}");
+        }
+    }
+}
